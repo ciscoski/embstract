4721efc6f61ca2336571545c6fbaafcf512fa347
ciscoski <francesco.cervellera@gmail.com>
2024-11-15 20:15:23 +0000

    refactor: Imrpove toolchain

--- tools/cmake/toolchains/gcc-arm-none-eabi.cmake
+++ tools/cmake/toolchains/gcc-arm-none-eabi.cmake
@@ -1,41 +1,31 @@
+set(CMAKE_CROSSCOMPILING true)
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-
-set(TRIPLET arm-none-eabi)
-set(CMAKE_C_COMPILER ${TRIPLET}-gcc)
-set(CMAKE_CXX_COMPILER ${TRIPLET}-g++)
-set(CMAKE_ASM_COMPILER ${TRIPLET}-gcc)
-set(CMAKE_CROSSCOMPILING true)
-
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
+# find the fully qualified path to the tool chain
+set(TOOLCHAIN_PREFIX arm-none-eabi-)
+find_program(TOOLCHAIN_C_COMPILER_PATH "${TOOLCHAIN_PREFIX}gcc" NO_CACHE REQUIRED)
+find_program(TOOLCHAIN_CXX_COMPILER_PATH "${TOOLCHAIN_PREFIX}g++" NO_CACHE REQUIRED)
+find_program(TOOLCHAIN_ASM_COMPILER_PATH "${TOOLCHAIN_PREFIX}gcc" NO_CACHE REQUIRED)
 
-function(target_generate_mapfile TARGET_NAME GENERATE)
-if(${GENERATE})
-    set(target_map_file_path "$<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.map")
-    target_link_options(${TARGET_NAME} PUBLIC -Xlinker -Map=${target_map_file_path})
-    set_property(TARGET ${TARGET_NAME}
-        APPEND
-        PROPERTY ADDITIONAL_CLEAN_FILES ${target_map_file_path})
-endif()
-endfunction()
+set(CMAKE_C_COMPILER ${TOOLCHAIN_C_COMPILER_PATH})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_CXX_COMPILER_PATH})
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_ASM_COMPILER_PATH})
+
+execute_process(COMMAND ${CMAKE_C_COMPILER} -print-sysroot
+    OUTPUT_VARIABLE CMAKE_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
+cmake_path(SET CMAKE_SYSROOT NORMALIZE ${CMAKE_SYSROOT})
 
 # Add a command to generate firmware in a provided format
 function(target_generate_object TARGET_NAME SUFFIX TYPE)
     set(target_object_file_path $<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}${SUFFIX})
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-        COMMAND ${TRIPLET}-objcopy -O ${TYPE}
+        COMMAND ${CMAKE_OBJCOPY} -O ${TYPE}
         "${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_NAME:${TARGET_NAME}>" ${target_object_file_path})
     set_property(TARGET ${TARGET_NAME}
                  APPEND
                  PROPERTY ADDITIONAL_CLEAN_FILES ${target_object_file_path})
-endfunction()
-
-#Assert if compiler older than required
-function(assert_compiler_version)
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS ${GCC_MIN_VERSION})
-        message(FATAL_ERROR "arm-none-eabi-gcc version must be ${GCC_MIN_VERSION} or greater. Found version ${CMAKE_C_COMPILER_VERSION}." )
-    endif()
 endfunction()
 
 # pclint settings
--- tools/cmake/toolchains/gcc-host.cmake
+++ tools/cmake/toolchains/gcc-host.cmake
@@ -1,29 +1,14 @@
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_ASM_COMPILER gcc)
+find_program(TOOLCHAIN_C_COMPILER_PATH "gcc" NO_CACHE REQUIRED)
+find_program(TOOLCHAIN_CXX_COMPILER_PATH "g++" NO_CACHE REQUIRED)
 
-function(target_generate_mapfile TARGET_NAME GENERATE)
-if(${GENERATE})
-    set(target_map_file_path "$<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.map")
-    target_link_options(${TARGET_NAME} PUBLIC -Xlinker -Map=${target_map_file_path})
-    set_property(TARGET ${TARGET_NAME}
-        APPEND
-        PROPERTY ADDITIONAL_CLEAN_FILES ${target_map_file_path})
-endif()
-endfunction()
+set(CMAKE_C_COMPILER TOOLCHAIN_C_COMPILER_PATH)
+set(CMAKE_CXX_COMPILER TOOLCHAIN_CXX_COMPILER_PATH)
 
 # Add a command to generate firmware in a provided format
 function(target_generate_object TARGET_NAME SUFFIX TYPE)
     message(WARNING "This toolchain does not generate other format.")
 endfunction()
 
-#Assert if compiler older than required
-function(assert_compiler_version)
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS ${GCC_MIN_VERSION})
-        message(FATAL_ERROR "arm-none-eabi-gcc version must be ${GCC_MIN_VERSION} or greater. Found version ${CMAKE_C_COMPILER_VERSION}." )
-    endif()
-endfunction()
 
 set(PCLINT_COMPILER_NAME gcc CACHE STRING "The name of the compiler to be used by pclint.")
-
 set(PCLINT_COMPILER_BIN "${CMAKE_C_COMPILER}" CACHE STRING "Path to the compiler")
